6154029bef792bd8de1705c00be9443bb9086137
Ravi <ravi.ameer@gmail.com>
2022-07-06 22:39:02 +0530

    Added comments

--- png_to_pdf/script.py
+++ png_to_pdf/script.py
@@ -1,3 +1,5 @@
+# This script will convert the list of images to PDF file.
+
 from PIL import Image
 
 image_1 = Image.open('C:\\Users\\kumar\\Pictures\\passports\\Ravi_pp_1.jpg')
@@ -6,6 +8,7 @@ image_2 = Image.open('C:\\Users\\kumar\\Pictures\\passports\\Ravi_pp_2.jpg')
 im_1 = image_1.convert('RGB')
 im_2 = image_2.convert('RGB')
 
+# Ignore the first file from above and add the rest of images to the list.
 image_list = [im_2]
 
 im_1.save(r'C:\\Users\\kumar\\Pictures\\passports\\Ravi_pp.pdf', save_all=True, append_images=image_list)
