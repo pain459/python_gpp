708817d273da1d0d0ee639f459f75cb4bd3a8a58
Ravi <ravi.ameer@gmail.com>
2022-07-24 19:20:16 +0530

    formatting of digital_clock.py

--- small_p/M1/digital_clock.py
+++ small_p/M1/digital_clock.py
@@ -10,22 +10,24 @@ from time import strftime
 root = Tk()
 root.title('Clock')
 
+
 # This function is used to
 # display time on the label
 def time():
-	string = strftime('%H:%M:%S %p')
-	lbl.config(text = string)
-	lbl.after(1000, time)
+    string = strftime('%H:%M:%S %p')
+    lbl.config(text=string)
+    lbl.after(1000, time)
+
 
 # Styling the label widget so that clock
 # will look more attractive
-lbl = Label(root, font = ('calibri', 40, 'bold'),
-			background = 'purple',
-			foreground = 'white')
+lbl = Label(root, font=('calibri', 40, 'bold'),
+            background='purple',
+            foreground='white')
 
 # Placing clock at the centre
 # of the tkinter window
-lbl.pack(anchor = 'center')
+lbl.pack(anchor='center')
 time()
 
 mainloop()
